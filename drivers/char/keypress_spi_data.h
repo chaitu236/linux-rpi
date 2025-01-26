9e36599b3c23b2c747f3761ee5b696543442ca13
Chaitanya Vadrevu <chaitanya973@gmail.com>
2025-01-25 21:10:03 -0600

    keypress_spi_data.h: Add pedal data
    Signed-off-by: Chaitanya Vadrevu <chaitanya973@gmail.com>

--- drivers/char/keypress_spi_data.h
+++ drivers/char/keypress_spi_data.h
@@ -11,5 +11,6 @@ typedef struct __attribute__((packed)) {
 	uint16_t magic;
 	uint32_t time;
 	keypress_spi_data_t data[NO_OF_SIMULTANEOUS_KEYS];
+	uint8_t pedal_data[3];
 	uint32_t crc;
 } keypress_spi_buffer_t;
